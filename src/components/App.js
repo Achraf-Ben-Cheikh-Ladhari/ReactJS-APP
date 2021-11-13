cdbca0f03a4e6c12f83ae7df9ca33a8f7d26be49
Achraf Ben Cheikh Ladhari <94150092+Achraf-Ben-Cheikh-Ladhari@users.noreply.github.com>
2021-11-13 01:15:46 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+import Cart from './Cart'
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
+
+function App() {
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />
+		</div>
+	)
+}
+
+export default App
---
+++ src/datas/plantList.js
@@ -0,0 +1,100 @@
+import monstera from '../assets/monstera.jpg'
+import lyrata from '../assets/lyrata.jpg'
+import pothos from '../assets/pothos.jpg'
+import succulent from '../assets/succulent.jpg'
+import olivier from '../assets/olivier.jpg'
+import basil from '../assets/basil.jpg'
+import mint from '../assets/mint.jpg'
+import calathea from '../assets/calathea.jpg'
+import cactus from '../assets/cactus.jpg'
+
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		bestSale: true,
+		light: 2,
+		water: 3,
+		cover: monstera,
+		price: 15
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		light: 3,
+		water: 1,
+		cover: lyrata,
+		price: 16
+	},
+
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		light: 1,
+		water: 2,
+		cover: pothos,
+
+		price: 9
+	},
+	{
+		name: 'calathea',
+		category: 'classique',
+		id: '4kk',
+		light: 2,
+		water: 3,
+		cover: calathea,
+
+		price: 20
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		light: 3,
+		water: 1,
+		cover: olivier,
+		price: 25
+	},
+
+	{
+		name: 'cactus',
+		category: 'plante grasse',
+		id: '8fp',
+		light: 2,
+		water: 1,
+		cover: cactus,
+		price: 6
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		bestSale: true,
+		light: 2,
+		water: 3,
+		cover: basil,
+		price: 5
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		light: 2,
+		water: 1,
+		cover: succulent,
+		price: 8
+	},
+
+	{
+		name: 'menthe',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: mint,
+		price: 4
+	}
+]
